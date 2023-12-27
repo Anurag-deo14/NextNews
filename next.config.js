443269ee4f3a51c0c36aacf8740bed25e5cd6826
Anurag Deo <anuragjee2020@gmail.com>
2023-12-27 13:57:47 +0530

    repairing files

--- next.config.js
+++ next.config.js
@@ -1,51 +1,33 @@
+// next.config.js
+
 const withOffline = require('next-offline');
 
 module.exports = withOffline({
-  workboxOpts: {
-    swDest: './service-worker.js', // Adjust the path to match your project structure
-    runtimeCaching: [
-      {
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'start-url',
-          expiration: {
-            maxEntries: 1,
-            maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
-          },
-        },
-        urlPattern: '/',
-      },
-      {
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'static-asset',
-          expiration: {
-            maxEntries: 50,
-            maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
-          },
-        },
-        urlPattern: /\.(?:js|css)$/,
-      },
-      {
-        handler: 'StaleWhileRevalidate',
-        options: {
-          cacheName: 'images',
-          expiration: {
-            maxEntries: 50,
-            maxAgeSeconds: 60 * 60 * 24 * 7, // 7 days
-          },
-        },
-        urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
-      },
-      // Add more caching strategies as needed
-    ],
-  },
-  // Other next.config.js options go here...
+  // Your original Next.js configurations go here
+
   publicRuntimeConfig: {
     GUARDIAN_API_KEY: process.env.GUARDIAN_API_KEY,
     GUARDIAN_API_URL: process.env.GUARDIAN_API_URL,
   },
+
   images: {
     domains: ['media.guim.co.uk'],
+  },
+
+  // Example: Add additional runtime caching for dynamic data (API responses)
+  workboxOpts: {
+    runtimeCaching: [
+      {
+        urlPattern:  /^http:\/\/localhost:3000\/article\/.*/,
+
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'offlineCache',
+          expiration: {
+            maxEntries: 200,
+          },
+        },
+      },
+    ],
   },
 });
